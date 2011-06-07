7e0e850eabd4a356c0ed41d921bda5ec6c45d0ae
Ian Davis <nospam@iandavis.com>
2011-06-07 17:01:50 +0100

    Added LICENCE, MANIFEST.in and .gitignore

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = 'rdfchangesets',
-    version = '1.0.5',
+    version = '1.0.6',
     description = 'A Python library for managing RDF changesets',
     author='Ian Davis',
     author_email='nospam@iandavis.com',
